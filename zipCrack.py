ecf0bc3d67a38a6d50261f68a0642bec1652e68a
Eric Henry <correia.eh@gmail.com>
2017-11-21 19:51:16 -0500

    update zip file cracker to take in arguments to the zip file location and the dictionart file location. That way this program can be more easily used

--- zipCrack.py
+++ zipCrack.py
@@ -1,5 +1,15 @@
 import zipfile
+import argparse
 from threading import Thread
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--zipfile', help='the location of zip folder you wish to crack')
+parser.add_argument('-d', '--dictionary', help='the location of the dictionary you would like to use to crack the zip folder', type=str)
+args = parser.parse_args()
+
+if not (args.dictionary and args.zipfile):
+    parser.print_help()
+    exit(0)
 
 def extractFile(zFile, password):
     bpass = password.encode('utf-8')
@@ -9,15 +19,14 @@ def extractFile(zFile, password):
     except Exception as e:
         pass
 
-def main():
-    zFile = zipfile.ZipFile("./evil.zip")
-    passFile = open('./dictionary.txt')
+def main(dict_location, zip_location):
+    zFile = zipfile.ZipFile(zip_location)
+    passFile = open(dict_location)
 
     for line in passFile.readlines():
         password = line.strip('\n')
         t = Thread(target=extractFile, args=(zFile, password))
-
         t.start()
         
 if __name__ == '__main__':
-    main()
+    main(args.dictionary, args.zipfile)
